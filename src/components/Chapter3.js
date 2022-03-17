84f6c65e210f0535cc9c06cfb6494dbb8c349642
Rahul Gupta <Rahul.Gupta@anz.com>
2022-03-18 01:04:20 +1100

    Added table

--- src/components/Chapter3.js
+++ src/components/Chapter3.js
@@ -5,6 +5,11 @@ import {
   Appear,
   Slide,
   Text,
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableCell,
 } from "spectacle";
 
 const Chapter3 = () => (
@@ -49,6 +54,110 @@ const Chapter3 = () => (
           <br></br>
           No a single knowledge-sharing approach is correct solution for all
           types of learning
+        </Text>
+      </Appear>
+    </Slide>
+
+    <Slide backgroundColor="backgroundColor">
+      <Heading fontSize="header" textAlign="left">
+        Setting the stage: Psychological safety
+      </Heading>
+      <Appear>
+        <Text fontSize="paragraph">
+          Mentorship<br></br>
+          <br></br>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{
+                    padding: "8px",
+                    fontSize: "28px",
+                    backgroundColor: "#333",
+                    color: "#ddd",
+                  }}
+                >
+                  Recommended Pattern (cooperative)
+                </TableCell>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{
+                    padding: "8px",
+                    fontSize: "28px",
+                    backgroundColor: "#333",
+                    color: "#ddd",
+                  }}
+                >
+                  Antipattern (adversarial)
+                </TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Basic questions or mistakes are guided in the proper direction
+                </TableCell>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Basic questions or mistakes are picked on, and the person
+                  asking the question is chastised
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Explainations are given with the intent of helping the person
+                </TableCell>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Explainations are given with the intent of showwing off one's
+                  own knowledge
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Responses are kind, patient, and helpful
+                </TableCell>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Responses are condescending, snarky and unconstructive
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Interactions are shared discussions for finding solutions
+                </TableCell>
+                <TableCell
+                  border="1px solid #aaa"
+                  style={{ padding: "5px", fontSize: "28px" }}
+                >
+                  Interactions are arguments with <i>winners</i> and{" "}
+                  <i>losers</i>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </Text>
       </Appear>
     </Slide>
